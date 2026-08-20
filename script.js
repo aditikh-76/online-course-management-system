50bb7d82608132c7a611a6808ada183695617042
aditikh-76 <aditiyaar7980@gmail.com>
2026-08-20 22:13:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Login button
+function login() {
+
+    alert("Login page will open soon!");
+
+}
+
+
+// Explore Courses button
+function showCourses() {
+
+    document.getElementById("courses").scrollIntoView();
+
+}
+
+
+// Enroll button
+function enroll(courseName) {
+
+    alert("You selected: " + courseName);
+
+}
